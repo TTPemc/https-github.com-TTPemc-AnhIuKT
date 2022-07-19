48c7c849a87649c7037a80296872461e44665316
TTPemc <93332560+TTPemc@users.noreply.github.com>
2022-07-19 22:50:29 +0700

    Initial Commit

---
+++ Generator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Generator
+//
+//  Created by Mac on 7/19/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
