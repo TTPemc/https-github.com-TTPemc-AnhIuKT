1dc48ea1a5aeb3f084d45becd8c235d07487008d
TTPemc <93332560+TTPemc@users.noreply.github.com>
2022-08-01 11:14:45 +0700

    Update main.cpp

--- Generator/main.cpp
+++ Generator/main.cpp
@@ -1,14 +1,52 @@
-//
-//  main.cpp
-//  Generator
-//
-//  Created by Mac on 7/19/22.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+                                      iKT:         Kim22Tuyen22KT2KT
+                                   .:KTKT2KTKTiuKimtuyen:         K2T
+                            .2IuAyameKTkhiconkimtuyenKymTuien:     KT.
+                          AyameKTKT::..     . . . .    .:k2KTKT2:.K2T
+                       tKTKT2t.      ..:.:.:.:.:.:::.:....  .tKT2KT:
+                    .KTKT2: ..        :::::::::::::::::::::... .kTKT2
+                   KTKT: ....:        ::::::::::::::::::::::::..  :KT2t
+                 KTKT. ...:::::.     .:::::::::::::::::::::::::.:.. tKTT
+                 KTKT. ...:::::.     .:::::::::::::::::::::::::.:.. tKTK
+               2KTt. ..:::::.:.:KTKT2:.:::::::::::::::::::::::::::.. .KTT2
+             .KT2  .::::::::..iuKT::KT:.:::::::::::::::::::::::::::::. iKT2
+             KT:   :::::::::.2KT     KT..:::::::::.:.:...:::::::::::::. iKT2
+            KT2   :::::::::..KT     :KT:.::::::...:.:.:::...:::::::::... iKT2
+            KT   ....::::::..KT    KTKT2.:::...:k2KtKtKtKt2t:..::::...    iKY
+           KT2 .IKT:..:::::..KT   .KTKT2..:.:iuKymTuien202KTkt..:.. .tk2KTcute
+           KT..KT.KT:.:::::..KT    KTKT2 ..2yeuKimTuyennhieulam:...iuKTKTKTKTKT:
+          .KT KT   KT ..:....KT.    2kt...anhyeunhieu22KimTuyen:.:KT2:.    .:2KT2
+          .KT.kt  KT2....:t:.:t:..:. . ..:iuKimTuyenhathoiiuiu2:.2KT  ......  .IKT
+          .KT.T. KTKT..kKTKt....:::::.:::.:KimTuyencute2xinhdep..:KT: :.:::::...IKT
+          .KT.T: 2KTt..Kt....:::::::::::::..:2AnhiuKimTuyenKT:.   IKT ...:.:::. iuKTnhieu:
+           KT.KT  t2:.....:::::::::::::::..   .:kt22KtT2ikt....KTKTKT2  . ..:::. IUKT202IKT
+           KT:Kt   ..:::::::::::::::::::.iuKT:  ...........:..tKTt:AnhiuKT2..:::.:KT     KT
+          KT2 .::::.:::::::::::::::::::..:iuKTkt..:.:.:.:::::.:KT.   2ktt2KT:.::. KT.    KT.
+         KT2t2:.::::::::::::::::::::.:.. .KTKTKT :::::::::::::.2KT2 .. ....:::::..KT2    KT
+        KT2k2kt.:::::::::::::::::::..  :KTKT:KT2..::::::::::::. :KT: .:.:.:.::::. KT:   2KT
+        KTKt2kt:.:::::::::::::::.:.. iKTKT:kt:iKT..::::::::::::: :IKT..:::::::::..KT2   KT
+       :KTT2Ktt.:::::.:::.:.:...  :IUKT2ktkt2ktKTt..::::::::::::: :KTt..::::::::: AYE  AYE
+       :KT2Aye.:::::.t......   :2KTKT2:kt2t222iuKT .:::::::::::::. .KT:..:::::::. AYT 2KT
+       :KT2Kti.:::::.t......   :2KTKT2:kt2t202ktKT .:::::::::::::. .KT:..:::::::. IKT 2KT
+        KTi::.:::::.:KYmtuIenktktkt:kymtuienkt2:KT2.:::::::::::::.. kKt.::::::::: KTT kT
+        KTt  :.:::::.iKTKTKTKTKTKTkimtuien222ktk:kt2..::::::::::::. KT2.::::::::. KTktK.
+        :KTt .:::::::.. .   2KTTKTT:tt22222t2tt:KT2 :::::::::::..  tkt:.:::::::..:KTKTKTKT
+         tKT: .:::::::...:..  :KTKt:t2t222t2t2:tKTt.::::::::..  .2KTTi :::::::...KT:   .IKT
+          KTTt .:.:::::::::.:.  iKT2:t2t2t2kt:iKTT..:::::..  .2KTKTTt :::::::...KTT     iKT.
+           KTKT. ..:::::::::::.. 2KTKt:t:t:tttKT2..:.:... .iKTKTT:tt ..:::::. :KTT     :KTt
+            2KTK2  ...:::::::::.. .TKTKTtKTKTKT:  ...  .tKTKTt2.  ..:.:::::. kkT    kKTKTt
+              KTKTT:   ....:.:.:...  iuKTlam.      .tKTKTKTt.  ..:.:::::::.tKTiu    .KTt
+                2KTKT22:.       .            .iuKTnhatKT:.. ..:::::::::::..KTkTKT.    KT
+                  :KTKTKTKTTkt2iuKTnhieulamKTKTKTKTT:. ..:.:::::::::::::::..   2KTKT2KTt
+                      ANHYEUKTNHIEUNHIEULAMk2t2:.  ...:...     .:::::::::::::.. 2IUKT2.
+                      KT2  ....:.:::.. KT: . .....:::.           .::::::::::::.. 2KT.
+                      KTT2:..........  KT.  . :::::       . .     :::::::::::::.. iKT.
+                      KTt  ..:.:::::..:KT      .t:   .   .   .    .:::::::::::::.. KTK
+                      KTY . . ..:.:.. iKT   .   .   . . .   . .   .::::::::::::::.. IKT
+                      :IUKT22T2.      IKT              .   .     .::::::::::::::::...IKT
+                        :tIUKIMTUYENnhieu   .   . . .   . . .    .:::::::::::::::::. iKT.
+                               :iuKTLAM22  . .     . . .   .    .:::::::::::::::::::..IKT
+                                      KT2     . . . .   . .   .:t::::::::::::::::::::.iKT2
+                                      iKT  . . . . . . . . . :kt:t:t:t:t:t:t:t:t:t:t::.IKT
+                                                                                          
+                                                                                        Anh Yêu Em!!
